936206b4c15612247aad2310699e7235b63d6fec
unknown <guilhermealves2596@gmail.com>
2021-02-26 00:37:51 -0300

    Criado a pesquisa
    Implementado relacionamentos entre as tabelas
    
    Criação de funcionalidade de envio de email com Node
    
    Criação de Template de Email

---
+++ api/src/controllers/SendMailController.ts
@@ -0,0 +1,82 @@
+import { Request, Response } from 'express';
+import { resolve } from 'path'; // Lib para facilitar mapeamento de elementos no nosso projeto
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UsersRepository';
+import { SurveysRepository } from '../repositories/SurveysRepository';
+import { SurveysUsersRepository } from '../repositories/SurveysUsersRepository';
+import SendMailService from '../services/SendMailService';
+
+class SendMailController {
+
+     async execute( request: Request, response: Response) {
+          const { email, survey_id } = request.body;
+
+          const usersRepository = getCustomRepository(UsersRepository);
+          const surveysRepository = getCustomRepository(SurveysRepository);
+          const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+
+          const userAlreadyExists = await usersRepository.findOne({email});
+
+          if(!userAlreadyExists) {
+               return response.status(400).json({
+                    error: "User does not exist",
+               });
+          }
+
+          const surveyAlreadyExists = await surveysRepository.findOne({ id: survey_id}); // Propriedade ID , valor SURVEY_ID
+
+          if(!surveyAlreadyExists) {
+               return response.status(400).json({
+                    error: "Survey does not exist",
+               });
+          }
+
+          const variables = {
+               name: userAlreadyExists.name,
+               title: surveyAlreadyExists.title,
+               description: surveyAlreadyExists.description,
+               user_id: userAlreadyExists.id,
+               link: process.env.URL_MAIL
+          };
+
+           // Pelo fato de importamos com => export default new SendMailService => Podemos usar da forma abaixo
+           const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");                     
+
+          const surveyUserAlreadyExists = surveysUsersRepository.findOne({
+               where: [
+                    { user_id: userAlreadyExists.id},
+                    { value: null }
+               ],
+               relations: ["user", "survey"] // Forma de recuperar o objeto completo, com o relacionamento, através do mapeamento nas Models
+          });
+
+          if(surveyUserAlreadyExists) {
+               await SendMailService.execute(
+                    email, 
+                    surveyAlreadyExists.title,
+                    variables,
+                    npsPath
+               );
+
+               return response.json(surveysUsersRepository);
+          }
+
+          // Salvar informações na table SurveysUsers || Enviar E-mail para o usuário 
+           const surveyUser = surveysUsersRepository.create({
+                user_id: userAlreadyExists.id,
+                survey_id 
+           })
+           await surveysUsersRepository.save(surveyUser);          
+
+           await SendMailService.execute(
+                email, 
+                surveyAlreadyExists.title,
+                variables,
+                npsPath 
+          );
+
+           return response.json(surveyUser);
+     }
+}
+
+export { SendMailController }
